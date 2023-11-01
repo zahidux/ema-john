3b071c56a69ad13fca2c3691209eed2927e1ab57
zahidux <zahidux@gmail.com>
2023-11-01 19:25:36 +0600

    last update

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,10 +7,13 @@ const Cart = ({cart}) => {
     // console.log(cart)
 
     let total = 0;
-    let totalShipping =0;
+    let totalShipping = 0;
+    let quantity = 0;
     for(const product of cart){
-        total = total +product.price;
+        product.quantity =product.quantity || 1;
+        total = total + product.price * product.quantity;
         totalShipping = totalShipping +product.shipping;
+        quantity = quantity + product.quantity;
     }
 
 
@@ -23,7 +26,7 @@ const Cart = ({cart}) => {
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
-                <p>Seleted items: {cart.length}</p>
+                <p>Seleted items: {quantity}</p>
                 <p>Total Price: ${total}</p>
                 <p>Total Shipping Charge: ${totalShipping}</p>
                 <p>Tax: ${tax.toFixed(2)}</p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,6 +4,7 @@ import './Shop.css'
 import { useEffect } from 'react';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 
 const Shop = () => {
     const [products, setProducts]= useState([]);
@@ -16,11 +17,36 @@ const Shop = () => {
         .then(data =>setProducts(data))
     },[])
 
+//products set in localStorage and get from localStorage
+    useEffect(()=>{
+        const storeCart = getShoppingCart();
+        const saveCart =[];
+        //step-1: get id that store in local storage
+        for(const id in storeCart){
+            //step 2: get the products by using id
+            const addProducts = products.find(product =>product.id ===id)
+            // console.log(addProducts)
+            //step 3: get quantity of the products
+            if(addProducts){
+                const quantity = setCart[id];
+            addProducts.quantity = quantity;
+            //step:4 add the addedproducts to the saved cart
+            saveCart.push(addProducts);
+            }
+            console.log(addProducts)
+
+        } 
+        //step-5: setCart
+        setCart(saveCart)
+
+    },[products])
+
 //add to card
     const addToCard = (product)=> {
         const newCart = [...cart, product]
         setCart(newCart);
         // console.log(cart)
+        addToDb(product.id);
     
     }
 
